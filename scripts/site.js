fb170f0533f1e223c077086785869be199bb1e0f
Grant Ramsay <seapagan@gmail.com>
2022-07-15 17:07:24 +0100

    impplement toggle price

--- scripts/site.js
+++ scripts/site.js
@@ -1,1 +1,10 @@
 console.log("Javascript Loaded");
+
+const toggle = document.getElementById("toggle");
+const prices = document.querySelectorAll(".price");
+
+const handleToggle = () => {
+  prices.forEach((price) => price.classList.toggle("active"));
+};
+
+toggle.addEventListener("click", handleToggle);
